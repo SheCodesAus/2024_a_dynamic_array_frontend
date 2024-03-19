c0cb7469a6d3694d3f6dab90095ac278900788e3
Ktech <ktechgau@gmail.com>
2024-03-19 19:23:05 +0800

    testing branch push v2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>TESTING - Karla</h1>
+        <h1>TESTING - Karla2</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
