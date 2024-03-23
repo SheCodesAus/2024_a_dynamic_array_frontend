dd14795e78c05e2e30149af8bcc063fbbcd3d732
lindi <lindi.harding@gmail.com>
2024-03-23 23:43:35 +0800

    added api for get-countries

---
+++ src/api/get-countries.js
@@ -0,0 +1,13 @@
+var headers = new Headers();
+headers.append("X-CSCAPI-KEY", "API_KEY");
+
+var requestOptions = {
+   method: 'GET',
+   headers: headers,
+   redirect: 'follow'
+};
+
+fetch("https://api.countrystatecity.in/v1/countries", requestOptions)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
