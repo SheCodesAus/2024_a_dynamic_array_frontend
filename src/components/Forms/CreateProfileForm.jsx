89c7bc238ae2fbb0afce99ccde4b82b5ca2cf8be
lindi <lindi.harding@gmail.com>
2024-03-23 23:08:32 +0800

    bugfix spelling error in post-profile API import into CreateProfileForm

--- src/components/Forms/CreateProfileForm.jsx
+++ src/components/Forms/CreateProfileForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import postProfile from "../../api/post-proile";
+import postProfile from "../../api/post-profile";
 
 import { useNavigate } from "react-router-dom"; // import the useNavigate hook
 import { useAuth } from "../../hooks/use-auth";
