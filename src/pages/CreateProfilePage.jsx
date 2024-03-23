1c705e3d5df2681228630de2d5d9a651b2837a00
lindi <lindi.harding@gmail.com>
2024-03-23 23:05:35 +0800

    added create profile page

---
+++ src/pages/CreateProfilePage.jsx
@@ -0,0 +1,10 @@
+import CreateProfileForm from "../components/Forms/CreateProfileForm.jsx";
+
+function CreateProfilePage() {
+  return (
+    <>
+      <CreateProfileForm />
+    </>
+  );
+}
+export default CreateProfilePage;
