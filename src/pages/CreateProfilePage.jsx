c640970719f7729a205be27920e2bd3ac91bfae9
Ktech <ktechgau@gmail.com>
2024-03-25 09:20:19 +0800

    fixed responsive issue mobile view signup and login forms

--- src/pages/CreateProfilePage.jsx
+++ src/pages/CreateProfilePage.jsx
@@ -1,5 +1,8 @@
 import CreateProfileForm from "../components/Forms/CreateProfileForm.jsx";
 
+import name from "../assets/NavBar/name.png";
+import "../components/CreateProfile/CreateProfile.css";
+
 function CreateProfilePage() {
   return (
     <>
