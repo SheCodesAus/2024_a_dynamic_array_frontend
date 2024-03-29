250987b5a088b775ee593900053313e32074d4a0
Sahar Kavousi <s.ka1366@gmail.com>
2024-03-29 16:22:26 +0800

    worked on the contact us page - css - mobile size

--- src/components/Forms/ContactFormWithHelp.jsx
+++ src/components/Forms/ContactFormWithHelp.jsx
@@ -4,7 +4,9 @@ import "../Forms/SignupForm.css";
 import "../Buttons/CtaButton.css";
 import EmailDropdownMenu from "../EmailDropdown/EmailDropdownMenu";
 
+
 function ContactFormWithHelp() {
+
   const [submitSuccess, setSubmitSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [helpFormData, setHelpFormData] = useState({
@@ -15,7 +17,7 @@ function ContactFormWithHelp() {
   });
 
   const handleChange = (e) => {
-    setHelpFormData({ ...helpFormData, [e.target.name]: e.target.value });
+    setHelpFormData({...helpFormData, [e.target.name]: e.target.value});
   };
 
   const handleSubmit = async (e) => {
@@ -67,59 +69,62 @@ function ContactFormWithHelp() {
   };
 
   return (
-    <>
-      <section className="contact-with-help-container">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Full Name</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              value={helpFormData.name}
-              onChange={handleChange}
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email address"
-              value={helpFormData.email}
-              onChange={handleChange}
-            ></input>
-          </div>
-          <div>
-            <EmailDropdownMenu
-              name="subject"
-              id="subject"
-              setSelectedSubject={(subject) =>
-                setHelpFormData({ ...helpFormData, subject })
-              }
-            />
-          </div>
-          <div>
+      <>
+        <section className="contact-with-help-container">
+
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Full Name</label>
+              <input
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                  value={helpFormData.name}
+                  onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                  type="text"
+                  name="email"
+                  placeholder="Enter your email address"
+                  value={helpFormData.email}
+                  onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="subject">Subject</label>
+              <EmailDropdownMenu
+                  name="subject"
+                  id="subject"
+                  setSelectedSubject={(subject) =>
+                      setHelpFormData({...helpFormData, subject})
+                  }
+              />
+            </div>
+            <div>
             <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Type your enquiry here"
-              value={helpFormData.message}
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <div>
-            <button className="userbutton">Submit</button>
-          </div>
-          <div className="submit-success">
-            {submitSuccess && (
-              <p> Thanks for reaching out! Your message was sent.</p>
-            )}
-          </div>
-        </form>
-      </section>
-    </>
+              <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Type your enquiry here"
+                  value={helpFormData.message}
+                  onChange={handleChange}
+              ></textarea>
+            </div>
+            <div>
+              <button className="userbutton">Submit</button>
+            </div>
+            <div className="submit-success">
+              {submitSuccess && (
+                  <p> Thanks for reaching out! Your message was sent.</p>
+              )}
+            </div>
+          </form>
+        </section>
+      </>
   );
 }
+
 export default ContactFormWithHelp;
