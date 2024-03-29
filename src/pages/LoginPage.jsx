e8bdbcf14bbb46ef2549224d36d037dc87dd0aee
Sahar Kavousi <s.ka1366@gmail.com>
2024-03-29 17:50:50 +0800

    worked on the login page - css - mobile and desktop size

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,19 @@
 import LoginForm from "../components/Forms/LoginForm";
 import loginSvg from "../assets/Illustrations/login.svg"
 import "../components/Login/Login.css";
-import name from "../assets/NavBar/name.png";
 
 function LoginPage() {
-  return (
-    <section className="login-main-container">
-        <img className="diversiTech-name-logo" src={name} alt="Diversitech name logo"/>
-        <img className="loginPageImage" src={loginSvg} alt="Diversitech name logo"/>
-        <div className="">
-            <LoginForm/>
-        </div>
-    </section>
-  );
+    return (
+        <section className="login-main-container">
+            <div className="login-page-image-container">
+                <img className="loginPageImage" src={loginSvg} alt="Diversitech name logo"/>
+            </div>
+
+            <div className="login-form-container">
+                <LoginForm/>
+            </div>
+        </section>
+    );
 }
+
 export default LoginPage;
