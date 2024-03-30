95828d9753e19935fdbb66bd9e0ec6ef2e7a1e84
Ktech <ktechgau@gmail.com>
2024-03-31 00:06:09 +0800

    feat: randomised cards

--- src/components/ProfileCards/ProfileCards.jsx
+++ src/components/ProfileCards/ProfileCards.jsx
@@ -1,10 +1,32 @@
 import "../../components/ProfileCards/ProfileCards.css";
 import ProfileCard from "./ProfileCard";
+import { useState, useEffect } from "react";
 
 function ProfileCards({ profiles }) {
+  const [shuffledProfiles, setShuffledProfiles] = useState([]);
+
+  useEffect(() => {
+    const allProfiles = profiles.flat();
+    const shuffled = shuffleArray(allProfiles);
+    setShuffledProfiles(shuffled);
+  }, []);
+
+  //the function to shuffle the array elements(Fisher-Yates algorithm)
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array]; //creates a copy of the original array
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ]; //swap the current element with a random element
+    }
+    return shuffledArray;
+  };
+
   return (
     <div className="profile-cards-container">
-      {profiles.map((profile, index) => (
+      {shuffledProfiles.map((profile, index) => (
         <ProfileCard key={index} profile={profile} />
       ))}
     </div>
