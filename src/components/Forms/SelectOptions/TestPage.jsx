bfb5c393a00c0514251da4e2e69544b778e18751
lindi <lindi.harding@gmail.com>
2024-03-31 17:56:26 +0800

    conditional rendering on test page

--- src/components/Forms/SelectOptions/TestPage.jsx
+++ src/components/Forms/SelectOptions/TestPage.jsx
@@ -1,19 +1,22 @@
 import CountrySelect from "./CountrySelect.jsx";
 import StateSelect from "./StateSelect.jsx";
+import CitySelect from "./CitySelect.jsx";
 import React, {useState} from "react";
 
 function TestPage() {
 
     const [stateIso2, setStateIso2] = useState("");
     const [countryIso2, setCountryIso2] = useState("");
+    const [cityIso2, setCityIso2] = useState("");
     
-    console.log("countryIso2: ", countryIso2);
-
     return (
         <div>
             <h1>Test Page</h1>
             <CountrySelect setCountryIso2={setCountryIso2} />
-            <StateSelect countryIso2={countryIso2} setStateIso2={setStateIso2}/>
+            { countryIso2 
+                && <StateSelect countryIso2={countryIso2} setStateIso2={setStateIso2}/>}
+            { stateIso2 
+                && <CitySelect cityIso2={cityIso2} setCityIso2={setCityIso2}/>}
         </div>
     );
 }
