29c9f9ef9fade67fe7461329fb672fd68e91ef51
Rachel Blake <blakerach1@gmail.com>
2024-03-31 11:29:44 +0800

    post profile form label fixes, error handling on create profile

--- src/api/get-user.js
+++ src/api/get-user.js
@@ -1,5 +1,8 @@
 async function getUser(username) {
     const url = `${import.meta.env.VITE_API_URL}/users/${username}`;
+    // to test in local: comment line above and uncomment line below (also check url in line below matches your local backend url)
+    // const url=`http://127.0.0.1:8000/users/${username}`;
+    
     const response = await fetch(url, { method: "GET" });
   
     if (!response.ok) {
