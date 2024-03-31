06ec3bb631904b6cd3be26caf6eb8e6ffe581e09
Rachel Blake <blakerach1@gmail.com>
2024-03-31 17:04:05 +0800

    still updating userpage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,18 @@
-// import useUser from "../hooks/use-user";
-
+import useUser from "../hooks/use-user";
 
 
 function userPage() {
+    const { user, isLoading, error} = useUser();
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        
+    }
+
+
     return <h1>This is a user page.</h1>;
 }
 
